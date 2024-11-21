7c7ab9fb7cfc654600a806cde835e1e2379cf6fd
Vanderson Batista <vandersonb@gmail.com>
2024-11-20 22:18:36 -0300

    Adicionado

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
-  imports: [],
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss'
+  styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent {
-
+  slides = [
+    { image: 'assets/img/advogado.jpg', title: 'Serviço 1', description: 'Consultoria Jurídica Especializada' },
+    { image: 'assets/img/advogado2.jpeg', title: 'Serviço 2', description: 'Defesa em Processo Judicial' },
+    { image: 'assets/img/advogados3.jpg', title: 'Serviço 3', description: 'Consultoria Trabalhista' }
+  ];
 }
