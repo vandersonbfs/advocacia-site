79be333c730702050c9ee17f9456f018c990b596
Vanderson Batista <vandersonb@gmail.com>
2024-11-20 22:33:30 -0300

    atualização

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-faq',
-  imports: [],
   templateUrl: './faq.component.html',
-  styleUrl: './faq.component.scss'
+  styleUrls: ['./faq.component.scss']
 })
 export class FaqComponent {
-
+  faqs = [
+    { question: 'Como agendar uma consulta?', answer: 'Você pode agendar uma consulta diretamente pelo nosso site ou entrando em contato por telefone.' },
+    { question: 'Quais serviços vocês oferecem?', answer: 'Oferecemos consultoria jurídica, defesa em processos judiciais, entre outros.' },
+  ];
 }
